3ebbbdd965ef313ef95fcfe80b016153fa4346a0
Will-Bee <78200129+Will-Bee@users.noreply.github.com>
2023-05-03 13:01:38 +0200

    Upload

---
+++ kbModule.py
@@ -0,0 +1,33 @@
+# Import whats needed to press keys from pynput
+import pydirectinput
+from time import sleep
+from random import randint
+
+
+
+key = "f"
+
+# Function to press a key for a certain amount of time
+def select():
+    pydirectinput.keyDown(key)
+    sleep(    randint(0, 2)/10    )
+    pydirectinput.keyUp(key)
+
+
+
+def change():
+    pydirectinput.keyDown(key)
+    sleep(    randint(13, 16)/10    )
+    pydirectinput.keyUp(key)
+    sleep(1)
+
+
+# TODO Implement into main code
+
+
+if __name__ == "__main__":
+    sleep(5)
+
+    change()
+    select()
+
---
+++ main.py
@@ -0,0 +1,154 @@
+import kbModule as kb
+from time import sleep
+
+### There are only 2 types of instruction
+### 1: Select digit (You can only move to left and from the last digit to the first digit)
+### 2: Change digit (You can only add 1 to the digit, if the digit is 9, it will be 0)
+### I need to go through every possible combination of the digits
+
+
+
+class CodeLock:
+
+    selectionIndex = 0
+
+    counter = 0
+
+    def __init__(self, digits):
+        self.type = digits
+        if digits == 3:
+            self.digits = [0, 0, 0]
+        elif digits == 4:
+            self.digits = [0, 0, 0, 0]
+
+    def changeDigit(self):
+        digitToChange = self.digits[self.selectionIndex]
+        if digitToChange == 9:
+            digitToChange = 0
+        else:
+            digitToChange = digitToChange + 1
+
+        self.digits[self.selectionIndex] = digitToChange
+
+        kb.change()
+        self.log()
+
+    def selectDigit(self):
+        if self.selectionIndex == self.type - 1:
+            self.selectionIndex = 0
+        else:
+            self.selectionIndex = self.selectionIndex + 1
+
+        kb.select()
+        self.log()
+
+    def unlock4(self):
+
+        for t in range(0, 10):
+            for h in range(0, 10-1):
+                self.selectDigit()
+                self.selectDigit()
+                self.selectDigit()
+
+
+                for d in range(0, 10):
+                    self.changeDigit()
+
+                    self.counter += 1
+
+
+                for t in range(0, 10):
+                    self.selectDigit()
+                    self.selectDigit()
+                    self.selectDigit()
+
+                    self.changeDigit()
+
+                    self. selectDigit()
+
+                    for d in range(0, 10):
+                        self.changeDigit()
+
+                        self.counter += 1
+
+                # 1 - 99
+
+                self.changeDigit()
+
+                self.selectDigit()
+                self.selectDigit()
+                self.changeDigit()
+                self.selectDigit()
+
+                self.changeDigit()
+
+                self.selectDigit()
+                self.selectDigit()
+
+                self.counter += 1
+
+            self.changeDigit()
+            self.selectDigit()
+            self.changeDigit()
+            self.selectDigit()
+            self.changeDigit()
+            self.selectDigit()
+            self.changeDigit()
+            self.selectDigit()
+            self.counter += 1
+
+
+
+    def bruteForce(self):
+
+        for i in range(5, 0, -1):
+            print("\r[Starting in ", i, "]", end="", sep="")
+            sleep(1)
+
+        print("\r                      ")
+        self.guiHeader()
+
+        if self.type == 3:
+            pass
+        elif self.type == 4:
+            self.unlock4()
+
+            print("Total combinations: " + str(self.counter))
+
+
+
+    def guiHeader(self):
+        # Cli:
+
+        ### DayZ Lockpicker ###
+        ### Code type: {X} digits ###
+        ### Lockpicking... ###
+        ###  0  [0]  0   0 
+
+
+
+
+        # move to the start of the line
+        print('\r', end='')
+        # print the bar
+        print("[ ] DayZ Lockpicker")
+        print("[ ] Code type: " + str(self.type) + " digits")
+        print("[ ] Lockpicking...")
+
+    def log(self):
+
+        for index, digit in enumerate(self.digits):
+            if index == self.selectionIndex:
+                print(f"[{self.digits[index]}]", end="")
+            else:
+                print(f" {self.digits[index]} ", end="")
+
+        print("\r", end="")
+
+if __name__ == "__main__":
+
+    # Make countdown
+
+
+    codeLock = CodeLock(4)
+    codeLock.bruteForce()
